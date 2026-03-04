b286d2c8f54575bd8bde707b81f53a1785cea3c9
Kaditya Rakan Pandyansa <166594666+CauchyFx@users.noreply.github.com>
2026-03-04 10:44:49 +0700

    Implement sensor calibration and file output
    This program calibrates sensor values and saves them to a file.

---
+++ praktikum_4/latihan_4.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream> // Pustaka akses file
+#include <string>  // Pustaka teks
+#include <vector>  // Pustaka vector
+
+int main() {
+
+    //- Tahap 1 -//
+    std::vector<double> sensor;
+
+    //- Tahap 2 -//
+    sensor.push_back(100.5);
+    sensor.push_back(200.0);
+    sensor.push_back(150.5);
+
+    //- Tahap 3 -//
+    for (size_t i = 0; i < sensor.size(); i++) {
+        sensor[i] *= 1.1;
+    }
+
+    //- Tahap 4 -//
+    std::ofstream fileOutput("hasil_kalibrasi.txt");
+
+    if (fileOutput.is_open()) {
+        //- Tahap 5 -//
+        for (size_t i = 0; i < sensor.size(); i++) {
+            fileOutput << sensor[i] << std::endl;
+        }
+        fileOutput.close();
+        
+        std::cout << "Successfully saved at 'hasil_kalibrasi.txt'\n";
+
+    } else {
+        std::cout << "Failed to open file.\n";
+    }
+
+    return 0;
+}
